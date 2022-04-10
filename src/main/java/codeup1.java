078e3ceb20c571fb61d5116a3bdb812a36f798d3
MR-KIMDAUN <people5474@naver.com>
2022-04-11 00:15:30 +0900

    20220411

---
+++ src/main/java/codeup1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class codeup1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int m = s.nextInt();
+
+        System.out.printf("%d %d", n, m);
+    }
+}
